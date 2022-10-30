151f78f6d230f1a01cc4e0b60596d5fae606bb32
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-30 09:54:06 +0000

    make new books add to library array, loop through array to add to page, and remove childNodes each time. Ability to click the bin and remove the book from library

--- script.js
+++ script.js
@@ -21,53 +21,62 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // add some books to library before user input
     const set1 = new Book("The Hobbit", "JRR Tolkein", 653, "https://m.media-amazon.com/images/I/8157XzPwdYL._AC_SY679_.jpg")
-    const set2 = new Book("Macbeth", "Shakespeare", 200, "https://d1b14unh5d6w7g.cloudfront.net/1853260355.01.S001.LXXXXXXX.jpg?Expires=1667062431&Signature=fzBBpqgWAUCVqsK2nKwzqotJRINP~mQQrWYMe8kvbyr79Lefimi45Xl74FmGNjxxI856t4PxAdvAG6R0lj~34YIbNJv3WtK0xR6~qwbHUOR653lx33kTnM1bk5--7dpuQXV4fvh8S7Myp7c3cftLbb29sxH84vH6LWElI6hDsRg_&Key-Pair-Id=APKAIUO27P366FGALUMQ")
+    const set2 = new Book("Macbeth", "Shakespeare", 200, "https://d1b14unh5d6w7g.cloudfront.net/0198324006.01.S001.LXXXXXXX.jpg?Expires=1667204477&Signature=BLj65Z6OC5x-7Kto3dlLsyGRki~nmPnCpE6Uy0jYOxAPkAlMHV~ihVArQvdrroVDIhfH3GrAUNEIBXcel9uDmrRck8v1i7GLbgJiS65znkIH0K5f4xlZsyaP-28ewdFQl4tO8U0oAzqJZPKTqN2rtkrTKj2L4mwPyB~6WJbcli4_&Key-Pair-Id=APKAIUO27P366FGALUMQ")
     library.push(set1, set2)
 
     // add list of library books to page
-    // if no input, function uses prebuilt library
-    // else will take the book they've added.
     function appendBook(Library = library){
-        // loop through books in library
-        if(Array.isArray(Library)){
-            //add to the web
-            Library.forEach( (newBook) => {
-                toWeb(newBook)
-            })
+        // loop through children of container and delete them
+        // to avoid duplication
+        while(container.firstChild){
+            container.removeChild(container.lastChild);
         }
-        else{
-            //add to the web
-            toWeb(Library)
-        }
+
+        // loop through library and append book
+        Library.forEach((book, index) =>{
+            toWeb(book, index)
+        })
+    }
+
+    // close pop up function
+    function closePopup(){
+        form.style.display = "none";
+        form.style.visibility = "hidden";
+        formback.style.display = "none";
+        formback.style.visibility = "hidden";
+        closeHolder.style.display = "none";
+        closeHolder.style.visibility = "hidden";
+        close.style.display = "none";
+        close.style.visibility = "hidden";
     }
 
     // Takes input and adds a book to library
     document.querySelector('button').addEventListener('click', function(){
         // get details from form
-        let title = document.getElementById('title').value;
-        let author = document.getElementById('author').value;
-        let pages = document.getElementById('pages').value;
-        let url = document.getElementById('url').value;
+        let title = document.getElementById('title');
+        let author = document.getElementById('author');
+        let pages = document.getElementById('pages');
+        let url = document.getElementById('url');
 
         // turn details into an object
-        let newBook = new Book(title, author, pages, url)
+        let newBook = new Book(title.value, author.value, pages.value, url.value)
         
         // add object to library
         library.push(newBook)
 
         // add book to page
-        appendBook(newBook)
-
+        appendBook(library)
+        closePopup()
         // clear input field
-        document.getElementById('title').value = ""
-        document.getElementById('author').value = ""
-        document.getElementById('pages').value = ""
-        document.getElementById('url').value = ""
+        title.value = ""
+        author.value = ""
+        pages.value = ""
+        url.value = ""
     })
 
     // append books one at a time to the webpage
-    function toWeb(newBook){
-        
+    function toWeb(newBook, index){
+        console.log(index)
         // get back the object details
         let title = newBook.title
         let author = newBook.author
@@ -115,9 +124,10 @@ document.addEventListener('DOMContentLoaded', function(){
         text.appendChild(bottom)
         bottom.appendChild(bin)
         // set SVG and Class
-        bin.outerHTML = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>'
-        bin.classList = "bin"
-        
+        bin.outerHTML = `<svg data-index="${index}" class="bin" style="width:24px;height:24px" viewBox="0 0 24 24"><path data-index="${index}" class="bin" fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z"/></svg>`
+
+        bin.dataset.index = index
+
         // bottom.appendChild(toggle)
 
         // add book to container
@@ -138,18 +148,19 @@ document.addEventListener('DOMContentLoaded', function(){
     })
 
     // close pop up box
-    document.querySelector(".close").addEventListener('click', ()=>{
-        // make visible and back form to center pop up.
-        form.style.display = "none";
-        form.style.visibility = "hidden";
-        formback.style.display = "none";
-        formback.style.visibility = "hidden";
-        closeHolder.style.display = "none";
-        closeHolder.style.visibility = "hidden";
-        close.style.display = "none";
-        close.style.visibility = "hidden";
-    })
+    document.querySelector(".close").addEventListener('click', () => closePopup())    
 
     // add existing library to page
     appendBook()
+
+    // check for bin, not intially
+    document.addEventListener('click', (event)=>{
+        if(event.target.className.baseVal == "bin"){
+            library.splice(event.target.dataset.index, 1)
+            appendBook(library)
+        }
+        // else if(){
+
+        //}
+    })
 })
